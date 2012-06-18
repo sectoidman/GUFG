f8f3eedf007dd9b32f832a58370b14c4d03ef543
Alex <eshink@gmail.com>
2012-06-18 16:08:37 -0700

    Slightly less ghetto framelocking algorithm

--- keytest.cc
+++ keytest.cc
@@ -11,10 +11,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <chrono>
+#include <thread>
 using namespace std;
 int main(int argc, char* argv[])
 {
 	/*Spawn the interface*/
+	typedef std::chrono::duration<int,std::micro> microsecs_t ;
+	std::chrono::high_resolution_clock::time_point frameStart;
+	std::chrono::high_resolution_clock::time_point frameEnd; 
 	interface game;
 	int rounds = 2;
 //	int count = 0;
@@ -23,18 +27,13 @@ int main(int argc, char* argv[])
 
 	/*Loop of everything*/
 	while (!game.gameover){
-		std::chrono::high_resolution_clock::time_point frameEnd; 
-		std::chrono::high_resolution_clock::time_point frameStart = std::chrono::high_resolution_clock::now(); 
+		frameStart = std::chrono::high_resolution_clock::now(); 
 		game.readInput();
 		game.resolve();
-		int dur;
-		typedef std::chrono::duration<int,std::micro> microsecs_t ;
-		do{
-			frameEnd = std::chrono::high_resolution_clock::now(); 
-			microsecs_t frameDuration( std::chrono::duration_cast<microsecs_t>(frameEnd - frameStart) ); 
-			dur = frameDuration.count();
-		} while (dur < 16667);
-		std::cout << "Frame time " << dur << " [us] " << std::endl; 
+		frameEnd = std::chrono::high_resolution_clock::now(); 
+		microsecs_t frameDuration( std::chrono::duration_cast<microsecs_t>(frameEnd - frameStart) );
+		std::chrono::microseconds sleepDuration(16667 - frameDuration.count()); 
+		std::this_thread::sleep_for(sleepDuration); 
 	}
 }
 
