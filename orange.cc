cb0d6bac48d5cffdcc05e826c22a727816c79db2
Alex Kelly <alex.page.kelly@gmail.com>
2012-08-13 07:23:41 -0700

    First round of orange projectile logic-ing

--- orange.cc
+++ orange.cc
@@ -12,3 +12,25 @@ void orange::init(action *& cMove)
 	matrixComplexity = 0;
 	matrix = NULL;
 }
+
+avatar * orange::spawn(action * source)
+{
+	avatar * n = source->spawn();
+	if(!n) return NULL;
+	else {
+		avatar ** temp; 
+		if(matrixComplexity < 6) temp = new avatar*[matrixComplexity+1];
+		else{
+			temp = new avatar*[6];
+			matrix[5]->dead = true;
+		}
+		int i;
+		for(i = matrixComplexity; i > 0; i--){
+			if(i != 6) temp[i] = matrix[i - 1];
+		}
+		temp[0] = n;
+		if(matrix) delete [] matrix;
+		matrix = temp;
+		return matrix[0];
+	}
+}
--- orange.h
+++ orange.h
@@ -1,30 +1,13 @@
 #ifndef ORANGE_INCLUDED
 #define ORANGE_INCLUDED
 #include "yellow.h"
-class beam : public projectile {
-public:
-	beam();
-	beam(const char*, const char*);
-	virtual void build(const char*, const char*);
-	virtual void init();
-	virtual action * createMove(char*);
-};
-
 class orange : public character {
 public:
 	orange();
 	void init(action *&);
 
-	beam * matrix;
+	avatar ** matrix;
 	int matrixComplexity;
-	avatar * spawn();
-};
-
-class beamStop : public special {
-public:
-	beamStop();
-	beamStop(const char*);
-	virtual void execute(action *, int *&);
-	virtual bool check(bool[], bool[], int, int, int[], SDL_Rect&); //Check to see if the action is possible right now.
+	avatar * spawn(action *);
 };
 #endif
