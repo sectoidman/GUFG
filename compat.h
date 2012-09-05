563e10923834e817394d486b2ed59c19a108450d
Alex Kelly <alex.page.kelly@gmail.com>
2012-09-05 15:18:46 -0700

    Removed more thread stuff

--- compat.h
+++ compat.h
@@ -4,13 +4,11 @@
   Nobody's written a decent free C++11 thread library implementation yet,
   so we'll have to just use boost in the mean time.  
 */
-#include <boost/thread.hpp>
 #include <boost/chrono.hpp>
 
 #else
 
 //other platforms can use the C++11 standard library
-#include <thread>
 #include <chrono>
 
 #endif
