2a78e7e2f13a6e42e0e036be32db12fa0a6b44b4
Phoebe Holland <phoebe.m.holland@gmail.com>
2012-09-05 15:34:21 -0700

    fixing mac unix-style build. haven't added boost yet

--- compat.h
+++ compat.h
@@ -1,16 +1,16 @@
-#ifndef _BOOST
+#ifdef _BOOST
 
 /*
   Nobody's written a decent free C++11 thread library implementation yet,
   so we'll have to just use boost in the mean time.  
 */
 
-#include <thread>
-#include <chrono>
-
-#else
 #include <boost/thread.hpp>
 #include <boost/chrono.hpp>
+
+#else
+#include <thread>
+#include <chrono>
 #endif
 
 //other platforms can use the C++11 standard library
