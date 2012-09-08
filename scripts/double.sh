331543a64f9cb196b9491d7b7a59aadfc79c54fb
Alex Kelly <alex.page.kelly@gmail.com>
2012-09-07 18:43:13 -0700

    Adjusted all damage. I think this is enough to playtest the rest out

--- scripts/double.sh
+++ scripts/double.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 awk '{
-	gain = match($0, /Gain/)
+	gain = match($0, /Damage/)
 	if(gain){
-		print $1, 2*$2
-		if($3) print " " $3, 2*$4
-		if($5) print " " $5, 2*$6
-		if($7) print " " $7, 2*$8
-		if($9) print " " $9, 2*$10
-		if($11) print " " $11, 2*$12
-		if($13) print " " $13, 2*$14
+		print $1, $2+$2/2
+		if($3) print " " $3, $4+$4/2
+		if($5) print " " $5, $6+$6/2
+		if($7) print " " $7, $8+$8/2
+		if($9) print " " $9, $10+$10/2
+		if($11) print " " $11, $12+$12/2
+		if($13) print " " $13, $14+$14/2
 	} else {
 		print $0
 	}
