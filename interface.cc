36122dc804af166b03b17fe1645928a3575556a2
Alex Kelly <alex.page.kelly@gmail.com>
2012-09-12 12:03:54 -0700

    I don' know

--- interface.cc
+++ interface.cc
@@ -355,9 +355,9 @@ void interface::resolve()
 			p[0]->dragBG(bg.x + wall, bg.x + screenWidth - wall) );
 		p[0]->checkCorners(floor, bg.x + wall, bg.x + screenWidth - wall);
 		p[1]->checkCorners(floor, bg.x + wall, bg.x + screenWidth - wall);
-		
+
 		unitCollision();
-		
+
 		if(p[0]->cMove->state[p[0]->connectFlag].i & 1 && p[0]->cMove != p[0]->pick()->airNeutral) 
 			p[0]->checkFacing(p[1]);
 		if(p[1]->cMove->state[p[1]->connectFlag].i & 1 && p[1]->cMove != p[1]->pick()->airNeutral) 
