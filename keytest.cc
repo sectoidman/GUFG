0ae1cd31806185f0f01f249a5a583ecd0b580e34
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-09-26 15:57:43 -0700

    this doesn't SEEM to have broken anything or actually changed the framerate

--- keytest.cc
+++ keytest.cc
@@ -1,43 +1,42 @@
-/*Key Test for GUFG
- *This tiny program is intended to do some of the initial SDL engine testing for
- *Project: Ground Up Fighting Game.
- *
+/**GUFG
  *Written by Alex Kelly in 2012
  *Under the MIT OSI license
  *For detailed licensing information, see the COPYING file in this directory.
- */
+ *This file runs a loop of everything that takes place in the GUFG engine. 
+ 
+ **/
 
-#include "interface.h"
 #include "compat.h"
-#include <unistd.h>
-#include <stdlib.h>
+#include "interface.h"
 #include <SDL/SDL_opengl.h>
+#include <stdlib.h>
+#include <unistd.h>
 using namespace internal;
 
 int main(int argc, char* argv[])
 {
-	/*Spawn the interface*/
-	typedef chrono::duration<int,micro> microsecs_t ;
+	/**GUFG uses the `chrono` component of stdlibc++
+	 * to frame lock the game**/
+	const int FPS = 60;
+	typedef chrono::duration<float,std::ratio<1,FPS>> frame_t ;
 	chrono::high_resolution_clock::time_point frameStart;
-	chrono::high_resolution_clock::time_point frameEnd;
 	interface game;
 	int rounds = 2;
-//	int count = 0;
 	if(argc > 1) rounds = atoi(argv[1]);
 	if(rounds > 0 && rounds < 10) game.numRounds = rounds;
 
-	/*Loop of everything*/
+	/** As long as the game doesn't detect events that should cause a "game over", it loops over a few basic resolutions of game events **/
 	while (!game.gameover){
 		frameStart = chrono::high_resolution_clock::now();
-		game.readInput();
+		game.readInput(); 
 		game.resolve();
-		int dur;
-		do {
-			frameEnd = chrono::high_resolution_clock::now();
-			microsecs_t frameDuration(chrono::duration_cast<microsecs_t>(frameEnd - frameStart));
-			microsecs_t duration(16667 - frameDuration.count());
-			dur = duration.count();
-		} while(dur > 0);
+		frame_t frameElapsed;
+		/*Now eat up the rest of the frame: 
+		 *Check the time elapsed thus far in a loop
+		 *until it is one frame_t unit.
+		 *Then go on to the next frame*/
+		do { frameElapsed = chrono::duration_cast<frame_t>(chrono::high_resolution_clock::now() - frameStart);
+		} while(frameElapsed.count() < 1);
 	}
     return 0;
 }
