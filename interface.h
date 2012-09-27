7f8659f66d8521d8bd3455017a9165fb2ce7a232
Alex Kelly <alex.page.kelly@gmail.com>
2012-09-27 07:24:00 -0700

    Ruh?

--- interface.h
+++ interface.h
@@ -25,7 +25,7 @@ public:
 	void resolve();		/*The main loop of the game*/
 	void draw();		/*The primary function in charge of drawing things on the screen.*/
 	void runTimer();	/*Currently just a decrementer. May always just be a decrementer.*/
-	void spriteInit();	/*Part of the rendering toolchain. Sets up sprites for drawing.*/.
+	void spriteInit();	/*Part of the rendering toolchain. Sets up sprites for drawing.*/
 	bool screenInit();
 	int drawGlyph(const char *, int, int, int, int, int);
 	bool screenInit(int, int);
