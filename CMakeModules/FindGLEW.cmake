16424bca99bf528845955a06758067ecee4d4045
Joseph Woodruff <jawoodru@cs.pdx.edu>
2012-10-04 03:39:44 -0700

    Oops, forgot to add some whitespace.

--- CMakeModules/FindGLEW.cmake
+++ CMakeModules/FindGLEW.cmake
@@ -26,7 +26,6 @@ IF (WIN32)
 				${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
 				${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
 				DOC "The GLEW library (64-bit)")
-
 	ELSE(NV_SYSTEM_PROCESSOR STREQUAL "AMD64")
 		FIND_LIBRARY( GLEW_LIBRARY
 				NAMES glew GLEW glew32 glew32s
@@ -35,36 +34,33 @@ IF (WIN32)
 				${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
 				${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
 				DOC "The GLEW library")
-
 	ENDIF(NV_SYSTEM_PROCESSOR STREQUAL "AMD64")
-
 ELSE (WIN32)
-
 	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-			/usr/include
-			/usr/local/include
-			/sw/include
-			/opt/local/include
-			${GLEW_ROOT_DIR}/include
-			DOC "The directory where GL/glew.h resides")
+		/usr/include
+		/usr/local/include
+		/sw/include
+		/opt/local/include
+		${GLEW_ROOT_DIR}/include
+		DOC "The directory where GL/glew.h resides")
 
 	FIND_LIBRARY( GLEW_LIBRARY
-			NAMES GLEW glew
-			PATHS
-			/usr/lib64
-			/usr/lib
-			/usr/local/lib64
-			/usr/local/lib
-			/sw/lib
-			/opt/local/lib
-			${GLEW_ROOT_DIR}/lib
-			DOC "The GLEW library")
+		NAMES GLEW glew
+		PATHS
+		/usr/lib64
+		/usr/lib
+		/usr/local/lib64
+		/usr/local/lib
+		/sw/lib
+		/opt/local/lib
+		${GLEW_ROOT_DIR}/lib
+		DOC "The GLEW library")
 ENDIF (WIN32)
 
 SET(GLEW_FOUND "NO")
 IF (GLEW_INCLUDE_PATH AND GLEW_LIBRARY)
-SET(GLEW_LIBRARIES ${GLEW_LIBRARY})
-SET(GLEW_FOUND "YES")
+	SET(GLEW_LIBRARIES ${GLEW_LIBRARY})
+	SET(GLEW_FOUND "YES")
 ENDIF (GLEW_INCLUDE_PATH AND GLEW_LIBRARY)
 
 
