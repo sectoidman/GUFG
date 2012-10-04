4b871bea7724898773c695b26c0c3c2ce9fe43a1
Alex Kelly <alex.page.kelly@gmail.com>
2012-10-04 03:09:50 -0700

    More headlessness work

--- src/engine/interface.cc
+++ src/engine/interface.cc
@@ -48,6 +48,7 @@ interface::interface()
 	read.close();
 	sf = scalingFactor;
 	assert(screenInit() != false);
+	loadMisc();
 
 	/*Initialize players.*/
 	for(int i = 0; i < 2; i++){
@@ -212,7 +213,6 @@ bool interface::screenInit()
 	glLoadIdentity();
 
 	initd = true;
-	loadMisc();
 	return true;
 }
 
