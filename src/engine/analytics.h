4aac71c3c750592a30480d44ad545da9f9ef5edc
Alex <eshink@gmail.com>
2012-10-07 12:22:42 -0700

    Tentative more analytic stuff. Very subject to change as it begins actually being implemented

--- src/engine/analytics.h
+++ src/engine/analytics.h
@@ -1,8 +1,8 @@
 #include "player.h"
 struct frame {
-	int dir;
-	int pos[5];
+	int dir:4;
 	bool neg[5];
+	int pos[5];
 	frame * next;
 };
 
@@ -10,4 +10,18 @@ class replay {
 public:
 	int selection[2];
 	frame * head[2];
+	frame * curr[2];
+	virtual void append(frame*);
+	virtual void load(const char*);
+	virtual void write();
+};
+
+class combo : public replay {
+public:
+	int posX, posY;
+	int proxX, proxY;
+	int damage;
+	int cost;
+	char * starter;
+	virtual void write();
 };
