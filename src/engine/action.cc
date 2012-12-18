e6fdc91c8e555270fb2c0f3dc4a6e0ece2000d3a
Alex Kelly <eshink@gmail.com>
2012-12-17 21:52:53 -0800

    Actually this default is probably better

--- src/engine/action.cc
+++ src/engine/action.cc
@@ -281,12 +281,13 @@ bool action::setParameter(char * buffer)
 		distortion->type = atoi(token);
 		token = strtok(NULL, "\t: \n");
 		distortion->radius = atoi(token);
-		token = strtok(NULL, "\t: \n");
-		distortion->ID = atoi(token);
 		return 1;
 	} else if (!strcmp("Attracts", token)) {
 		token = strtok(NULL, "\t: \n");
 		distortion->effectCode = atoi(token);
+		token = strtok(NULL, "\t: \n");
+		distortion->ID = atoi(token);
+		return 1;
 	} else if (!strcmp("Next", token)) {
 		token = strtok(NULL, "\t: \n");
 		tempNext = new char[strlen(token)+1];
