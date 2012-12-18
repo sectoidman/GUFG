786b9078689badc3293ee3f8838b872e874a1537
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-12-17 17:08:09 -0800

    I think this should undo all the gutting from earlier, which, believe me, will happen again in a more controlled fashion

--- src/c++/engine/interface.h
+++ src/c++/engine/interface.h
@@ -13,29 +13,68 @@
 #include "analytics.h"
 #ifndef INTERFACE
 #define INTERFACE
-
-
-
 class gameInstance{
 public:
+	void readInput();		/*Accepts input into input containers, for use by anything that wants it*/
 	virtual void resolve() = 0;	/*Every game type requires a resolve loop.*/
+	int drawGlyph(const char *, int, int, int, int, int);
+	virtual void loadMisc();
+	bool screenInit();
+	bool screenInit(int, int);
 
+//Input layer stuff. Players, inputs, etc.
+	player * p[2];
+	bool * sAxis[2];
+	int * posEdge[2];
+	bool * negEdge[2];
+
+//Meta-interface stuff. Gameover state, screen size, etc.
+	SDL_Surface *screen;
 	int screenHeight, screenWidth, floor, wall;
 	int freeze;
+	bool gameover;
+	bool fullscreen;
+	float scalingFactor, sf;
+	bool initd:1;
+	GLuint glyph[91];
 };
 
 class interface : public gameInstance{
 public:
 	interface();
 	~interface();
+	void loadMisc();
+	void createPlayers();
+	void createDemons();
+	void createDemons(replay*);
+	void startGame();
+	void loadMatchBackground();
+/*Important interface functions that will remain interface functions*/
+
+/*The main game loop runs readInput() and resolve() each exactly once per frame loop.*/
 	void genInput();
 	void resolve();		/*The main loop of the game*/
 	void draw();		/*The primary function in charge of drawing things on the screen.*/
 	void drawGame();
+	void drawCSelect();
+	void drawMainMenu(int);
+	void drawRematchMenu();
 	void cleanup();
+	void runTimer();	/*Currently just a decrementer. May always just be a decrementer.*/
+	void spriteInit();	/*Part of the rendering toolchain. Sets up sprites for drawing.*/
+	void roundInit();
+	void matchInit();
+	void cSelectMenu();
+	void mainMenu(int);
+	void keyConfig();
+	void rematchMenu();
+	void checkWin();
 	void dragBG(int);
 	void doSuperFreeze();
 	void unitCollision();
+	void readMatchupChart();
+	void writeMatchupChart();
+	void initialConfig(int);
 	void resolveHits();
 	void resolveThrows();
 	void resolveSummons();
@@ -43,8 +82,8 @@ public:
 	void cullThing(int);
 	void addAttractor(attractor*);
 	void cullAttractor(int);
+	void writeImage(const char*, int, action*);
 
-	GLuint background;
 	bool select[2];
 	int selection[2];
 	int menu[2];
@@ -66,7 +105,30 @@ public:
 	int grav;	//Gravitational constant. 
 	instance ** things;
 	int thingComplexity;
+
+	GLuint background;
+	Mix_Music *matchMusic;
+	Mix_Music *menuMusic;
+	Mix_Chunk **announceWinner;
+	Mix_Chunk *announceRound[3];
+	Mix_Chunk *announceDraw[2];
+	Mix_Chunk *announceFight;
+	Mix_Chunk *announceEnd[2];
+	Mix_Chunk *announceSelect;
+
 	attractor ** globals;
 	int attractorComplexity;
+
+//Variables for cSelectMenu 
+	int numChars;
+	GLuint selectScreen; 
+	SDL_Rect wheel;
+	int counter[2];
+
+	bool q;
+	int timer;
+	bool roundEnd:1;
+	int endTimer;
+	replay * currentMatch;
 };
 #endif
