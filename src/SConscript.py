05820a909ffe2be2e8a1c003c34a8d7d3eb68465
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-12-18 19:24:15 -0800

    makes a working dynamically linked gufg with game.cc. STILL can't get cython working quite right. need to adjust LD_LIBRARY_PATH for this to work

--- src/SConscript.py
+++ src/SConscript.py
@@ -1,23 +1,43 @@
-import distutils.sysconfig
 from os.path import realpath
+import distutils.sysconfig
+from re import compile as regex
 
+##SOURCES
 engine_src = Glob("c++/engine/*.cc")
 char_src = Glob("c++/characters/*.cc")
-swig_src = Glob("c++/engine/*.i")
+cython_src = Glob("python/*.p{yx,xd}")
+print cython_src
+##STUFF FOR BUILDING CYTHON
+pyxcpp = Builder(action='cython --cplus $SOURCE')
+def cythonLibrary(env, name, source, **kwargs):
+  cpp = env.PyxCpp(source)
+  env.SharedLibrary(name, cpp, **kwargs)
 
-env = Environment(CPPPATH=[distutils.sysconfig.get_python_inc()],
-                  SHLIBPREFIX="_",
+##ENVIRONMENT
+env = Environment(BUILDERS={'PyxCpp':pyxcpp},
+                  PYEXT_USE_DISTUTILS=True,
+                  CPPPATH=[distutils.sysconfig.get_python_inc()],
                   CXXFLAGS="-O2 -g\
                             -Wno-overloaded-virtual\
                             -Wno-format\
                             -Wno-format-contains-nul\
                             -Wctor-dtor-privacy\
                             -std=c++11")
-env.LoadableModule('engine', engine_src + char_src + swig_src,
-            LIBS=['engine',
-                  'SDL',
-                  'SDLmain',
-                  'SDL_image',
-                  'SDL_mixer',
-                  'GL'],
-            LIBPATH=['.'], SWIGFLAGS="-c++ -python")
+
+env.AddMethod(cythonLibrary, 'CythonLibrary')
+
+libs = ['engine',
+        'SDL',
+        'SDLmain',
+        'SDL_image',
+        'SDL_mixer',
+        'GL'],
+
+#TARGETS
+env.SharedLibrary('engine', engine_src + char_src)
+env.CythonLibrary("api", cython_src,
+                  LIBS=libs,
+                  LIBPATH=["."])
+Program('../gufg', 'c++/game.cc',
+            LIBS=libs,
+            LIBPATH=["."])
