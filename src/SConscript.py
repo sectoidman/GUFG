a23da64e61be26fee86d9dd8e044529fff4fa0f1
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-12-20 15:42:40 -0800

    okay this might work

--- src/SConscript.py
+++ src/SConscript.py
@@ -5,6 +5,12 @@ engine_src = File(filter(lambda s: not("wrap" in s),
                          Glob("c++/engine/*.cc", strings=True)))
 char_src = Glob("c++/characters/*.cc")
 swig_src = Glob("c++/engine/*.i")
+ #srcs should be SCons-provided File() objects or iterators of those
+ #you can get a list using Glob or just put a known File into a list.
+bmpout_src = File("c++/bmp.cc")
+replay_src = File("c++/replay.cc")
+gufg_exec_src = File("c++/game.cc")
+
 
 ##ENVIRONMENT
 env = Environment(PYEXT_USE_DISTUTILS=True,
@@ -17,24 +23,29 @@ env = Environment(PYEXT_USE_DISTUTILS=True,
                             -Wctor-dtor-privacy\
                             -std=c++11")
 
-libs = ['SDL',
-        'SDLmain',
-        'SDL_image',
-        'SDL_mixer',
-        'GL']
+libs=['SDL','SDLmain','SDL_image','SDL_mixer','GL']
 
 miscdirs = [".config", ".data"]
 for d in miscdirs:
   Execute(Mkdir(d))
 wrappy = "c++/engine/engine.py"
 
-
 #TARGETS
-engine = env.SharedLibrary('engine', engine_src + char_src + swig_src,
-                   LIBS=libs, LIBPATH=["."], SHLIBPREFIX="_")
+engine = env.SharedLibrary('engine', engine_src + char_src)
+runtime = env.SharedLibrary('runtime', engine + swig_src, SHLIBPREFIX="_",LIBS=libs)
+
+#Then specify a Program or SharedLibrary as a method of the env we set up,
+#these take a name, then an iterable list of SCons file objects
+#(or file iterables)
+#which can include source and objects we assigned earlier, such as engine
+bmpout = env.Program('bmpout', [engine, bmpout_src], LIBS=libs)
+replay = env.Program('replay', [engine, replay_src], LIBS=libs)
+gufg_exec = env.Program('gufg', [engine, gufg_exec_src], LIBS=libs)
+
+
 #INSTALL
 env.Install(source=[wrappy,
-                    engine,
+                    runtime,
                     Glob("../src/python/*.py")],
             target="runtime")
 
@@ -42,7 +53,10 @@ env.Install(source=["runtime",
                     "../content",
                     "../src/scripts",
                     Glob("../info/*"),
+                    bmpout,
+                    replay,
+                    gufg_exec,
                     miscdirs],
             target="../dist")
 
-env.InstallAs(target='../dist/gufg', source='../src/scripts/gufg.py')
+env.InstallAs(target='../dist/gufg.py', source='../src/scripts/gufg.py')
