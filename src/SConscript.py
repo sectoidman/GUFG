994cf2602a23136375a8c4654d54239a2fd272b6
H Forrest Alexander <h.forrest.alexander@gmail.com>
2012-12-20 16:35:22 -0800

    I think this fixed it

--- src/SConscript.py
+++ src/SConscript.py
@@ -19,21 +19,19 @@ env = Environment(PYEXT_USE_DISTUTILS=True,
                             -Wno-format\
                             -Wno-format-contains-nul\
                             -Wctor-dtor-privacy\
-                            -std=c++11")
-
-libs=['SDL','SDLmain','SDL_image','SDL_mixer','GL']
-
+                            -std=c++11",
+                  SHLIBPREFIX="_",
+                  LIBS=['SDL','SDLmain','SDL_image','SDL_mixer','GL'])
 miscdirs = [".config", ".data"]
 for d in miscdirs:
   Execute(Mkdir(d))
 wrappy = "c++/engine/engine.py"
 
 #TARGETS
-runtime = env.SharedLibrary('engine', backend_src + swig_src,
-                            SHLIBPREFIX="_", LIBS=libs)
-bmpout = env.Program('bmpout', backend_src + bmpout_src, LIBS=libs)
-replay = env.Program('replay', backend_src + replay_src, LIBS=libs)
-gufg_exec = env.Program('gufg',backend_src + gufg_exec_src, LIBS=libs)
+runtime = env.SharedLibrary('engine', backend_src + swig_src,)
+bmpout = env.Program('bmpout', backend_src + bmpout_src)
+replay = env.Program('replay', backend_src + replay_src)
+gufg_exec = env.Program('gufg',backend_src + gufg_exec_src)
 
 
 #INSTALL
