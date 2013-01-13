dbe54d1afcd1245d25c967e843996f3fa0a445ee
Alex Kelly <eshink@gmail.com>
2013-01-12 20:10:21 -0800

    Harness added

---
+++ harness.h
@@ -0,0 +1,8 @@
+#include <SDL/SDL.h>
+#ifndef HARNESS_INCLUDED
+#define HARNESS_INCLUDED
+class harness{
+public:
+	void init();
+};
+#endif
--- modelview.cc
+++ modelview.cc
@@ -1,11 +1,14 @@
 #include "window.h"
+#include "harness.h"
 int main(int argc, char * argv[])
 {
 	window mainWin;
+	harness joy;
 	if( !mainWin.screenInit() ){
 		fprintf( stderr, "Failed to initialize\n" );
 		return -1;
 	}
+	joy.init();
 	while(true);
 	return 1;
 }
