cc86a19e485e3a5506a5e60b9acdbb5d78bdbe69
Alex Kelly <eshink@gmail.com>
2013-01-12 20:32:47 -0800

    Abstracted this

--- harness.cc
+++ harness.cc
@@ -11,31 +11,36 @@ void harness::init()
 		SDL_JoystickOpen(i);
 }
 
+void harness::processInput(SDL_Event &event)
+{
+	/*Do stuff with event*/
+	switch (event.type){
+	/*Kill handler*/
+	case SDL_QUIT:
+		printf("Sigterm received!\n");
+		gameover = 1;
+		break;
+	/*Keyboard handler. Maybe I'll optimize such that the knows if it even needs to check this (EG if sticks are used)*/
+	case SDL_KEYDOWN:
+		switch (event.key.keysym.sym) {
+		case SDLK_q:
+		case SDLK_ESCAPE:
+			gameover = 1;
+			break;
+		default:
+			break;
+		}
+		break;
+	}
+}
+
 void harness::readInput()
 {
 	/*Make our dummy event for polls*/
 	SDL_Event event;
 	for(int i = 0; i < 20; i++){
 		if (SDL_PollEvent(&event)){
-			/*Do stuff with event*/
-			switch (event.type){
-			/*Kill handler*/
-			case SDL_QUIT:
-				printf("Sigterm received!\n");
-				gameover = 1;
-				break;
-				/*Keyboard handler. Maybe I'll optimize such that the knows if it even needs to check this (EG if sticks are used)*/
-			case SDL_KEYDOWN:
-				switch (event.key.keysym.sym) {
-				case SDLK_q:
-				case SDLK_ESCAPE:
-					gameover = 1;
-					break;
-				default:
-					break;
-				}
-				break;
-			}
+			processInput(event);
 		}
 	}
 }
