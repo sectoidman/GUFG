c70b9afba8fb5e235ea56c333da4ca5bf5ff490b
Alex Kelly <eshink@gmail.com>
2013-01-12 19:59:27 -0800

    Fixed

--- modelview.cc
+++ modelview.cc
@@ -3,7 +3,7 @@ int main(int argc, char * argv[])
 {
 	window mainWin;
 	if( !mainWin.screenInit() ){
-		fprintf( stderr, "Failed to initialize GLFW\n" );
+		fprintf( stderr, "Failed to initialize\n" );
 		return -1;
 	}
 	while(true);
