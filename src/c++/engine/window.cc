0812f40e74c5c54be26cee5fd7de1bef7a6e4e3f
Alex Kelly <eshink@gmail.com>
2013-01-15 09:47:14 -0800

    ?

--- src/c++/engine/window.cc
+++ src/c++/engine/window.cc
@@ -29,6 +29,7 @@ bool window::screenInit()
 	glHint (GL_POLYGON_SMOOTH_HINT, GL_NICEST);
 
 	glClearColor(0, 0, 0, 0);
+	glShadeModel (GL_FLAT);i
 	glClearDepth(1.0f);
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
