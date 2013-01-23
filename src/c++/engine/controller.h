df5f8a2e85590dca7948b9b37c9671869b279ff0
Alex Kelly <alex.page.kelly@gmail.com>
2013-01-23 14:13:00 -0800

    Added 'tap'

--- src/c++/engine/controller.h
+++ src/c++/engine/controller.h
@@ -15,6 +15,7 @@ public:
 //	virtual bool readConfig(int);
 	virtual bool setKey(int, SDL_Event);
 	virtual bool same(SDL_Event);
+	virtual int tap(SDL_Event);
 	virtual void swapKey(int, SDL_Event);
 	std::vector<keySetting*> input;
 	std::vector<const char*> inputName;//Input names. This is really just for housekeeping.
