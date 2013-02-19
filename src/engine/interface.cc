eb810a19103f994699aeaaf71357db46cce97930
Alex Kelly <alex.page.kelly@gmail.com>
2013-02-19 00:54:24 -0800

    Made parrying *way* better

--- src/engine/interface.cc
+++ src/engine/interface.cc
@@ -1177,15 +1177,15 @@ void interface::resolveHits()
 			s[hitBy[i]].damage *= prorate[things[hitBy[i]]->ID-1];
 			if(actuallyDoesDamage && s[hitBy[i]].damage == 0) s[hitBy[i]].damage = 1;
 			hit[hitBy[i]] = things[i]->takeHit(combo[things[hitBy[i]]->ID-1], s[hitBy[i]], prox);
-			if(i < P.size() && hitBy[i] < (int)P.size()){
+			if(i < P.size()){
 				if(things[i]->particleType == -2){
 					hStat ths;
-					ths.damage = 1+s[hitBy[i]].chip;
+					ths.damage = s[hitBy[i]].chip ? s[hitBy[i]].chip : 1;
 					ths.ghostHit = true;
 					ths.stun = 0;
 					ths.push = s[hitBy[i]].push;
 					if(things[i]->current.aerial) ths.push += s[hitBy[i]].blowback*5;
-					P[(things[hitBy[i]]->ID)-1]->takeHit(combo[i], ths, prox);
+					P[things[hitBy[i]]->ID-1]->takeHit(combo[i], ths, prox);
 				}
 			}
 			if(i < P.size() && s[hitBy[i]].stun) combo[(i+1)%2] += hit[hitBy[i]];
