3afb571f2a1b443fd939d9fe61e271c4a8a29087
Alex Kelly <eshink@gmail.com>
2013-03-03 16:59:38 -0800

    Joystick support now works, full stop
    Conflicts:
    	src/engine/controller.cc
    	src/engine/player.cc

--- src/engine/player.cc
+++ src/engine/player.cc
@@ -751,8 +751,14 @@ void controller::readEvent(SDL_Event & event, frame &t)
 	int effect = tap(event);
 	if(effect != 0){
 		for(int i = 0; i < 4; i++){
-			if(abs(effect) & (1 << i)){ 
-				t.axis[i] = (effect > 0);
+			if(abs(effect) & (1 << i)){
+				if(effect > 0){
+					t.axis[i] = 1;
+				} else {
+					t.axis[i] = 0;
+				}
+				if(i%2 == 0) t.axis[i+1] = 0;
+				else t.axis[i-1] = 0;
 			}
 		}
 		for(unsigned int i = 0; i < t.pos.size(); i++){
