675be8d22c66ac99997da995d112e2c421737ae2
Phoebe M Holland <phoebe.m.holland@gmail.com>
2013-03-08 01:18:37 -0800

    these headers are in a stupid place on mac

---
+++ src/engine/gl-compat.h
@@ -0,0 +1,10 @@
+#ifdef ___MAC_OSX
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#endif
+#ifndef ___MAC_OSX
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
