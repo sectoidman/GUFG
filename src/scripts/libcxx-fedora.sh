2fb24531ed7a90e17c690cb41b9add282ce86389
H Forrest Alexander <h.forrest.alexander@gmail.com>
2013-03-11 01:42:26 -0700

    duh, now this works

--- src/scripts/libcxx-fedora.sh
+++ src/scripts/libcxx-fedora.sh
@@ -4,12 +4,13 @@ su -c "yum install libstdc++-static clang"
 git clone git://github.com/llvm-mirror/libcxx.git
 mkdir libcxx-build; cd libcxx-build
 
-stdcxx="/usr/include/$(gcc --version | head -n 1 | cut -d ")" -f 2 | cut -d " " -f 2)"
+stdcxx="/usr/include/c++/$(gcc --version | head -n 1 | cut -d ")" -f 2 | cut -d " " -f 2)"
+stdcxxpaths="$stdcxx;$stdcxx/x86_64-redhat-linux"
+echo $stdcxxpaths
 CC=clang \
 CXX=clang++ \
 cmake -DLIBCXX_CXX_ABI=libsupc++ \
-      -DLIBCXX_LIBSUPCXX_INCLUDE_PATHS="$stdcxx;
-                                        $stdcxx/x86_64-redhat-linux"\
+      -DLIBCXX_LIBSUPCXX_INCLUDE_PATHS=$stdcxxpaths\
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=/usr/local \
       ../libcxx
